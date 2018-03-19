1bafd734125c37c763ce85193eae8370ae62e5d7
jbentaha <jaouad.bentahar@gmail.com>
2018-03-19 23:20:27 +0100

    exprimer les codes en String

--- extract-TLD.py
+++ extract-TLD.py
@@ -13,15 +13,23 @@ def separate(data):
 
 # remplacer nom-du-fichier-index par le nom d'un fichier (cdx-00000, cdx-00001 etc)
 input_data = ""
-with open('nom-du-fichier-index.txt') as fi:
+with open('nom-du-fichier-index') as fi:
     input_data = fi.read()
 
 # prendre tout ce qui est entre timestamp et status http, par exemple :
 # 20180219115239 {"url": "http://example.com/", "mime": "text/html", "mime-detected": "text/html", "status": "301"
 data = re.findall('(?:[a-z])+(?:,[a-z]+)+.+\d{14}.+status": "\d{3}', input_data) # prendre les donnees en format "timestamp
 data_separated = list(map(lambda x : separate(x), data)) # appliquer la fonction du haut aux donnees
-
+code = ''
 with open('infos-TLD.csv', 'w') as fo:
     for i in range(0, len(data_separated)):
-        fo.write("{}, {}, {}\n".format(data_separated[i][0], data_separated[i][1], data_separated[i][2]))
+        if(int(data_separated[i][2]) >= 200 and int(data_separated[i][2]) < 300):
+            code = 'OK'
+        else:
+            if(int(data_separated[i][2]) >= 300 and int(data_separated[i][2]) < 400):
+                code = 'Redirection'
+            else:
+                if(int(data_separated[i][2]) >= 400):
+                    code = 'Erreur'
+        fo.write("{}, {}, {}\n".format(data_separated[i][0], data_separated[i][1], code))
     fo.close()
